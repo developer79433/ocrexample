9d65b2422b36bf43bf885d6d18bf315d9fb20eba
Android Developer <developer79433@gmail.com>
2016-10-11 21:35:25 +1100

    Iterate over regions of text detected by Tesseract, instead of just getting one big blob of text from it.

--- ocr.cpp
+++ ocr.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 void ocr(const char *filename)
 {
-    char *outText;
-
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English, without specifying tessdata path
     if (api->Init(NULL, "eng")) {
@@ -21,12 +19,28 @@ void ocr(const char *filename)
     // Open input image with leptonica library
     Pix *image = pixRead(filename);
     api->SetImage(image);
+#ifdef SIMPLE
     // Get OCR result
-    outText = api->GetUTF8Text();
+    char *outText = api->GetUTF8Text();
     cout << "OCR output:" << endl << outText << endl;
+    delete [] outText;
+#else
+    api->Recognize(0);
+      tesseract::ResultIterator* ri = api->GetIterator();
+      tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
+      if (ri != 0) {
+        do {
+          const char* word = ri->GetUTF8Text(level);
+          float conf = ri->Confidence(level);
+          int x1, y1, x2, y2;
+          ri->BoundingBox(level, &x1, &y1, &x2, &y2);
+          cout << "word: '" << word << "';  \tconf: " << conf << "; BoundingBox: " << x1 << "," << y1 << "," << x2 << "," << y2 << ";" << endl;
+          delete[] word;
+        } while (ri->Next(level));
+      }
+#endif
 
     // Destroy used object and release memory
     api->End();
-    delete [] outText;
     pixDestroy(&image);
 }
