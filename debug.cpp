df42fff39ee2001d60ef0e04645dec7b0eb0b22f
Android Developer <developer79433@gmail.com>
2016-10-12 01:13:27 +1100

    Output image channels and depth in OpenCV CV_8UC3 etc format.

--- debug.cpp
+++ debug.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
 
+#include "util.h"
+
 using namespace std;
 using namespace cv;
 
@@ -36,9 +38,34 @@ template <typename T> void dump_point_vector(const string &name, vector<Point_<T
 	cerr << endl;
 }
 
+static const char *depth_names[] = {
+	"CV_8U",
+	"CV_8S",
+	"CV_16U",
+	"CV_16S",
+	"CV_32S",
+	"CV_32F",
+	"CV_64F"
+};
+
+/**
+ * Docs say:
+ * CV_MAKETYPE(depth, n) == (depth&7) + ((n-1)<<3)
+ * This means that the constant type is formed from the depth, taking the lowest 3 bits, and the number of channels minus 1, taking the next log2(CV_CN_MAX) bits.
+ */
+#define TYPE_TO_DEPTH_INDEX(type) ((type) & 7)
+#define DEPTH_IDX_TO_DEPTH_NAME(idx) (((idx) >= 0 && static_cast<unsigned>(idx) < ELEMENTSOF(depth_names)) ? depth_names[(idx)] : "unknown")
+#define TYPE_TO_DEPTH_NAME(type) DEPTH_IDX_TO_DEPTH_NAME(TYPE_TO_DEPTH_INDEX(type))
+#define TYPE_TO_CHANNELS(type) (((type) >> 3) + 1)
+
 void display_image(const string &name, const Mat &image)
 {
-	cerr << "Image " << name << " dims: " << image.size().width << "x" << image.size().height << endl;
+	cerr
+	<< "Image " << name
+	<< " dims: " << image.size().width << "x" << image.size().height
+	<< " channels: " << image.channels()
+	<< " depth: " << TYPE_TO_DEPTH_NAME(image.type())
+	<< endl;
 	imshow(name, image);
 	waitKey(0);
 	destroyWindow(name);
